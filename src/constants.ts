d716157cd3fec3dd834c85a9aec8cbb26ec05206
Matthew Miller <matthew@millerti.me>
2020-01-18 23:11:11 -0800

    Move constants and logger into helpers/
    Also adds getLogTag and LogTag interface to logger

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const {
-  DISCORD_BOT_TOKEN,
-  PAPERTRAIL_HOST,
-  PAPERTRAIL_PORT,
-} = process.env;
-
-export const CMD_GROUPS = {
-  PUBLIC: 'public',
-};
---
+++ src/helpers/logger.ts
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+import pino from 'pino';
+import { SnowflakeUtil } from 'discord.js';
+
+export interface LogTag {
+  // eslint-disable-next-line camelcase
+  req_id: string;
+}
+
+export const logger = pino({
+  level: 'debug',
+});
+
+/**
+ * A helper method to generate an object that can be used to tag logged messages generated from
+ * a singular "request". This will help with troubleshooting sequences events by allowing for
+ * filtering on this particular, consistent ID.
+ */
+export function getLogTag (id?: string): LogTag {
+  let req_id;
+
+  if (id) {
+    // If an ID is provided then use that
+    req_id = id;
+  } else {
+    // Otherwise generate a snowflake as an ID
+    // See https://discordapp.com/developers/docs/reference#snowflakes
+    req_id = SnowflakeUtil.generate();
+  }
+
+  return { req_id };
+}
