9a48dbb940cbec446377dcaf98a0f6c8de4a0ba9
Matthew Miller <matthew@millerti.me>
2020-01-18 22:42:30 -0800

    Add working bot with custom help and embed

---
+++ src/bot.ts
@@ -0,0 +1,64 @@
+import { CommandoClient, SQLiteProvider } from 'discord.js-commando';
+import sqlite from 'sqlite';
+import path from 'path';
+
+import { DISCORD_BOT_TOKEN, CMD_GROUPS } from './constants';
+import { logger } from './logger';
+
+const bot = new CommandoClient({
+  commandPrefix: '<',
+  owner: '',
+  disabledEvents: [
+    'TYPING_START',
+  ],
+});
+
+// Set up a SQLite DB to preserve guide-specific command availability
+sqlite.open(path.join(__dirname, '../settings.db'))
+  .then(db => bot.setProvider(new SQLiteProvider(db)))
+  .catch(error => { logger.error('Error loading SQLite DB:', error); });
+
+// Initialize commands and command groups
+bot.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    [CMD_GROUPS.PUBLIC, 'For Everyone'],
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    unknownCommand: false,
+    help: false,
+    prefix: false,
+  })
+  // Automatically load commands that exist in the commands/ directory
+  // A custom filter is specified so that the `require-all` library picks up .ts files during dev
+  .registerCommandsIn({
+    dirname: path.join(__dirname, 'commands'),
+    filter: /^([^.].*)\.[jt]s$/,
+  });
+
+bot.once('ready', () => {
+  if (!bot.user) {
+    logger.error('Bot initialized but has no user, how did this happen?');
+    throw new Error('Bot initialized but has no user, how did this happen?');
+  }
+
+  logger.info('KIRBOT GO! <(^.^)>');
+  logger.info(`Logged in as ${bot.user.tag}`);
+  logger.info(`Command prefix: ${bot.commandPrefix}`);
+
+  bot.user.setActivity('<(\'.\'<)');
+});
+
+// Handle errors
+bot.on('error', (err) => {
+  if (err.message === 'Cannot read property \'trim\' of undefined') {
+    // Swallow a bug in discord.js-commando at:
+    // node_modules/discord.js-commando/src/extensions/message.js:109:28
+  } else {
+    logger.error(err, 'Bot system error');
+  }
+});
+
+// Start the bot
+bot.login(DISCORD_BOT_TOKEN);
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const {
+  DISCORD_BOT_TOKEN,
+  PAPERTRAIL_HOST,
+  PAPERTRAIL_PORT,
+} = process.env;
+
+export const CMD_GROUPS = {
+  PUBLIC: 'public',
+};
