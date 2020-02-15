d736593ad3776b484d4c989c8ed5e54e4c6cf842
Matthew Miller <matthew@millerti.me>
2020-02-15 14:35:58 -0800

    Build bot image in start-prod script

--- start-prod.sh
+++ start-prod.sh
@@ -5,5 +5,7 @@ echo "[---stopping prod bot---]"
 docker-compose stop bot
 echo "[---removing containers---]"
 docker-compose rm -f
+echo "[---building prod bot---]"
+docker-compose build bot
 echo "[---restarting prod bot---]"
 docker-compose -f docker-compose.yml up -d --no-deps bot
