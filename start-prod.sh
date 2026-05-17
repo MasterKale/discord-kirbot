6cb7c5ef6de220dcf02e1c37c781dcd2a0a1ceac
Matthew Miller <matthew@millerti.me>
2026-05-17 10:38:23 -0700

    Use "docker compose" now

--- start-prod.sh
+++ start-prod.sh
@@ -2,10 +2,10 @@
 echo "[---pulling latest code---]"
 git pull
 echo "[---stopping prod bot---]"
-docker-compose stop bot
+docker compose stop bot
 echo "[---removing containers---]"
-docker-compose rm -f
+docker compose rm -f
 echo "[---building prod bot---]"
-docker-compose build bot
+docker compose build bot
 echo "[---restarting prod bot---]"
-docker-compose -f docker-compose.yml up -d --no-deps bot
+docker compose -f docker-compose.yml up -d --no-deps bot
